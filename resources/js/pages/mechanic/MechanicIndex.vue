40ec93ad116cb81926a11636721b7b3e04abb5ac
Luiz Eduardo <luiztsilva@protonmail.com>
2026-05-18 17:33:56 -0300

    fix: ajusta layout das paginas autenticadas para usar max-w-7xl centralizado

--- resources/js/pages/mechanic/MechanicIndex.vue
+++ resources/js/pages/mechanic/MechanicIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
     <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-8">
       <div>
         <h1 class="text-2xl font-bold text-gray-900">Mecânicos</h1>
